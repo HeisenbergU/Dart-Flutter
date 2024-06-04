73f5ebf85be71c82ba4da8778614ede35f30880e
HeisenbergU <mehdixanli402@gmail.com>
2024-06-04 15:37:32 -0700

    Dart_2_Data_Types_Variables

---
+++ Dart_2_Data_Types_Variables/datataypes_variables.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  int yas = 34;
+  double ortalama = 5.6;
+  num il = 1998;
+
+print(yas);
+print(ortalama);
+print(il);
+
+
+String ad = "Omer";
+print(ad);
+
+  print("Omar");
+  print(ad + " sevdiyim rengler");
+  print(ad + " yasi");
+
+bool isAgreed = true;
+bool dontAgreed = false;
+
+print(isAgreed);
+print(dontAgreed);
+
+
+var name = "hero";
+
+var nameNumber = 1953;
+
+var haveMoney = true;
+
+print(name);
+
+print(nameNumber);
+
+print(haveMoney);
+
+
+}
