6736c7a37c08df7cd1afef3d06295297eb5e86c8
HeisenbergU <mehdixanli402@gmail.com>
2024-06-04 17:16:08 -0700

    Dart_3_Null_Safety_Data_Types

---
+++ Dart_3_Null_Safety_Data_Types/num_boolean.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  int yas = 34;
+  print(yas);
+
+  yas = 44;
+  print(yas);
+
+num il = 1998;
+print(il);
+
+
+double say = 52;
+print(say);
+
+int kilo = 84.6.toInt();
+
+print(kilo);
+
+var say2  = 56;
+
+int number;
+
+number = 5;
+
+print(number + 5);
+
+int? sl = null;
+
+sl = 9;
+
+print(sl + 5);
+
+int hexadecimalSay = 0xAABBBCC;
+
+print(hexadecimalSay);
+
+
+}
