26d98da804f738fb9caf312562b3e7cb08d2859c
HeisenbergU <mehdixanli402@gmail.com>
2024-06-05 03:09:21 -0700

    Dart_5_Task_1-4

---
+++ Dart_5_Task_1-4/task.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String name = "Omar";
+var surname = "Mehdikhanli";
+int age = 21;
+
+print("My name is $name $surname, age $age and my name charecter size: ${name.length + surname.length}'dir");
+
+int firstSide = 3;
+num secondSide = 4;
+var thirdSide = 5;
+
+print("first side $firstSide,second side $secondSide,third side $thirdSide total sum ${firstSide+secondSide+thirdSide}");
+
+}
